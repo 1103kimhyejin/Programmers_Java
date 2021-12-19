a9963464abe84f4114954764b3485803232f6852
1103kimhyejin <1103kimhyejin@gmail.com>
2021-12-20 00:48:32 +0900

    기능개발 - 스택/큐를 이용하여 조건에 맞는 배열 리턴

--- level2/Test211219.java
+++ level2/Test211219.java
@@ -50,28 +50,28 @@ public class Test211219 {
 		Queue<Integer> q = new LinkedList<>();
 		List<Integer> answerList = new ArrayList<>();
 
-        for (int i = 0; i < speeds.length; i++) {
-            double remain = (100 - progresses[i]) / (double) speeds[i];
-            int date = (int) Math.ceil(remain);
-
-            if (!q.isEmpty() && q.peek() < date) {
-                answerList.add(q.size());
-                q.clear();
-            }
-
-            q.offer(date);
-        }
-
-        answerList.add(q.size());
-
-        int[] answer = new int[answerList.size()];
-
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = answerList.get(i);
-        }
-        for(int i=0; i<answer.length; i++) {
-        	System.out.println(answer[i]);
-        }
+			for (int i = 0; i < speeds.length; i++) {
+	            double remain = (100 - progresses[i]) / (double) speeds[i];
+	            int date = (int) Math.ceil(remain);
+	
+	            if (!q.isEmpty() && q.peek() < date) {
+	                answerList.add(q.size());
+	                q.clear();
+	            }
+	
+	            q.offer(date);
+	        }
+	
+	        answerList.add(q.size());
+	
+	        int[] answer = new int[answerList.size()];
+	
+	        for (int i = 0; i < answer.length; i++) {
+	            answer[i] = answerList.get(i);
+	        }
+	        for(int i=0; i<answer.length; i++) {
+	        	System.out.println(answer[i]);
+	        }
 	}
 }
 //큐를 다 이용하면 비운다.
